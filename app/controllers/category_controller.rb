78045bb423da290c0594c66cec11521f324fe990
kade042 <dereje88@gmail.com>
2016-06-30 21:01:32 +0000

    finshr project

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,8 @@
+class CategoryController < ApplicationController
+
+  get '/category/:id' do
+    
+    @category = Category.find_by(id: params[:id])
+    erb :'posts/show_post'
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,82 @@
+class PostsController < ApplicationController
+
+  get '/posts' do
+    if logged_in?
+      erb :"posts/posts"
+    else
+      redirect "/login"
+    end
+
+  end
+
+  post '/posts' do
+
+    if logged_in? && !params[:title].empty? && !params[:content].empty?
+      @posts = Post.create(title: params[:title], content: params[:content], user_id: current_user.id)
+      @posts.category = Category.create(name: params[:category])
+      @posts.save
+
+      redirect "/posts"
+    else
+      redirect "/posts/new"
+    end
+  end
+
+  get '/posts/new' do
+    #binding.pry
+    if logged_in?
+      erb :"posts/create_post"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/posts/:id' do
+    if logged_in?
+      @post = Post.find_by(id: params[:id])
+      erb :"posts/edit_post"
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/posts/:id' do
+    #binding.pry
+    @post = Post.find_by(id: params[:id])
+    if logged_in? && !params["post"]["content"].empty?
+
+      cat = Category.find_by(id: @post.category.id)
+      cat.update(name: params[:category])
+      @post.update(params["post"])
+      @post.category = cat
+      #binding.pry
+      @post.save
+      redirect '/posts'
+    else
+      redirect "/posts/#{@post.id}/edit"
+    end
+  end
+
+  get '/posts/:id/edit' do
+    if logged_in?
+      @tweet = Post.find_by(id: params[:id])
+      erb :"posts/edit_post"
+    else
+      redirect '/login'
+    end
+
+  end
+
+  delete '/posts/:id/delete' do
+    if logged_in?
+      @user = current_user.posts
+      @post = @user.find_by(id: params[:id])
+      @post.delete if !@post.nil?
+      redirect  '/posts'
+    else
+      redirect "/login"
+    end
+
+  end
+
+end
